2b333f8ce275aa402832f7a4f0b17cad58bdfd4e
drilonbrahaa <drilonbraha@gmail.com>
2025-07-25 17:00:18 +0200

    Configured methods for querying database

--- src/main/java/com/example/blog/repositories/PostRepository.java
+++ src/main/java/com/example/blog/repositories/PostRepository.java
@@ -1,6 +1,7 @@
 package com.example.blog.repositories;
 
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,8 @@ import com.example.blog.entities.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByTitleContaining(String keyword);
+
+    List<Post> findByCategory_Name(String category);
+
+    List<Post> findDistinctByTags_NameIn(Set<String> tags);
 }
